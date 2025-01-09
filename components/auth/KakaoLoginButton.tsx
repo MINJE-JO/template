5860d1040702cab71ab051547c106c190fc29ba8
MINJE JO <jo@MINJEs-MacBook-Air.local>
2025-01-09 12:23:42 +0900

    no kakao image

--- components/auth/KakaoLoginButton.tsx
+++ components/auth/KakaoLoginButton.tsx
@@ -26,11 +26,6 @@ export default function KakaoLoginButton() {
       onClick={handleKakaoLogin}
       className="flex items-center justify-center w-full px-4 py-2 text-white bg-[#FEE500] hover:bg-[#E6CF00] rounded-lg"
     >
-      <img
-        src="/kakao-logo.png"
-        alt="Kakao"
-        className="w-5 h-5 mr-2"
-      />
       카카오로 로그인
     </button>
   );
